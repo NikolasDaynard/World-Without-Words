e1e546bc10c1d44707a45961e718d479d9e7d3e7
Nikolas Daynard <Nkdaynard@gmail.com>
2024-10-12 15:22:02 -0400

    rebind and some silly shader

--- entities/player/PlayerActions.gd
+++ entities/player/PlayerActions.gd
@@ -3,12 +3,6 @@ extends Node2D
 @onready var ui = $"../InteractionUi"
 @onready var characterControlller = $"CharacterBody2D"
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed == true:
-			ui.summon_text("SPIKE YEAHHHH\n oh ma gawd", 
-				characterControlller.global_position - Vector2(20, 20),
-				characterControlller.facing_direction)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,4 +10,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if Input.is_action_just_pressed("attack"):
+		ui.summon_text("SPIKE YEAHHHH\n oh ma gawd", 
+			characterControlller.global_position - Vector2(20, 20),
+			characterControlller.facing_direction)
 	pass
