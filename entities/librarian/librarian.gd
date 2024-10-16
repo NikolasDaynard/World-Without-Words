e1fa5edaa1d58b05ac44b45fff044ba47644e451
Nikolas Daynard <Nkdaynard@gmail.com>
2024-10-16 11:22:32 -0400

    camera lock

--- entities/librarian/librarian.gd
+++ entities/librarian/librarian.gd
@@ -17,12 +17,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# attackTimer += delta
-	if attackTimer > 4:
+	attackTimer += delta
+	if attackTimer > 10:
 		attackTimer = 0
+		health = -1
 		ui.summon_text("SPIKE YEAHHHH\n oh ma gawd", 
 			global_position + Vector2(70, 70),
 			(player.global_position - global_position).normalized().x,
 			false)
 
 	pass
+
+func is_alive() -> bool:
+	return health > 0
--- scripts/CameraLock.gd
+++ scripts/CameraLock.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
-export var endLock : Node2D
+@export var endLock: Node2D
+var locked_camera: bool = false
+var cameraRef: Camera2D
+var playerRef: CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +11,22 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	if endLock.has_method("is_alive"):
+		if not endLock.is_alive():
+			if locked_camera:
+				locked_camera = false
+				cameraRef.global_position = playerRef.global_position
+			cameraRef = null
+	if locked_camera and cameraRef is Camera2D:
+		cameraRef.global_position = global_position
 	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.get_node("Camera2D").global_position = global_position
+		locked_camera = true
+		cameraRef = body.get_node("Camera2D")
+		playerRef = body
+	pass # Replace with function body.
